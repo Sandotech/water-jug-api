a7ff8a1ec16bcf60be233effe129d37a1f102a1b
Diego Santos <dseosantos76@gmail.com>
2024-11-03 14:07:36 -0400

    📝 Add: swagger doc endpoint

--- src/interface/routes/waterJugRoutes.ts
+++ src/interface/routes/waterJugRoutes.ts
@@ -14,6 +14,85 @@ const waterJugRepository = new WaterJugRepository(waterJugSolverImpl);
 // Inject the repository into WaterJugController
 const waterJugController = new WaterJugController(waterJugRepository);
 
+/**
+ * @swagger
+ * /solve:
+ *   post:
+ *     summary: Solve the water jug problem
+ *     description: Solves the water jug problem given the capacities of two jugs and the desired amount of water.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               x_capacity:
+ *                 type: integer
+ *                 description: The capacity of the first jug.
+ *                 example: 4
+ *               y_capacity:
+ *                 type: integer
+ *                 description: The capacity of the second jug.
+ *                 example: 3
+ *               z_amount_wanted:
+ *                 type: integer
+ *                 description: The desired amount of water.
+ *                 example: 2
+ *     responses:
+ *       200:
+ *         description: Solution found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 solution:
+ *                   type: array
+ *                   description: The steps to solve the problem.
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       jug1:
+ *                         type: integer
+ *                         description: The amount of water in the first jug.
+ *                       jug2:
+ *                         type: integer
+ *                         description: The amount of water in the second jug.
+ *       400:
+ *         description: Invalid input
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: Invalid input. All inputs must be positive integers.
+ *       404:
+ *         description: No solution found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: No solution found.
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: An unexpected error occurred.
+*/
 router.post('/', (req, res) => waterJugController.solve(req, res));
 
 export default router;
