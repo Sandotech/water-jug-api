96e12e9c0221e9532341480438ec5e350e8c89ec
Diego Santos <dseosantos76@gmail.com>
2024-11-03 12:32:49 -0400

    🐛 Remove any and refact controller

--- src/interface/controllers/WaterJugController.ts
+++ src/interface/controllers/WaterJugController.ts
@@ -8,32 +8,46 @@ export class WaterJugController {
         this.repository = repository;
     }
 
-    solve(req: Request, res: Response): Response | any {
+    solve(req: Request, res: Response): Response {
         const { x_capacity, y_capacity, z_amount_wanted } = req.body;
 
-        try {        
+        try {
             // Validate inputs
-            if (typeof x_capacity !== 'number' || typeof y_capacity !== 'number' || typeof z_amount_wanted !== 'number') {
-                return res.status(400).json({ error: 'All inputs must be numbers.' });
+            if (!this.validateInputs(x_capacity, y_capacity, z_amount_wanted)) {
+                return res.status(400).json({ error: 'Invalid input. All inputs must be positive integers.' });
             }
 
-            if (!Number.isInteger(x_capacity) || !Number.isInteger(y_capacity) || !Number.isInteger(z_amount_wanted)) {
-                return res.status(400).json({ error: 'All inputs must be integers.' });
-            }
-
-            if (x_capacity <= 0 || y_capacity <= 0 || z_amount_wanted <= 0) {
-                return res.status(400).json({ error: 'All inputs must be positive integers.' });
-            }
-
+            // Solve the water jug problem using the repository
             const solution = this.repository.solve(x_capacity, y_capacity, z_amount_wanted);
 
             if (!solution) {
                 return res.status(404).json({ error: 'No solution found.' });
             }
 
-            res.status(200).json({ solution });
+            // Return the solution
+            return res.status(200).json({ solution });
         } catch (error) {
-            res.status(500).json({ error: 'An unexpected error occurred.' });            
+            // Handle unexpected errors
+            return res.status(500).json({ error: 'An unexpected error occurred.' });
         }
+    }
+
+    /*
+        Consists in validate if the inputs are positive Integers
+    */
+    private validateInputs(x_capacity: any, y_capacity: any, z_amount_wanted: any): boolean {
+        if (typeof x_capacity !== 'number' || typeof y_capacity !== 'number' || typeof z_amount_wanted !== 'number') {
+            return false;
+        }
+
+        if (!Number.isInteger(x_capacity) || !Number.isInteger(y_capacity) || !Number.isInteger(z_amount_wanted)) {
+            return false;
+        }
+
+        if (x_capacity <= 0 || y_capacity <= 0 || z_amount_wanted <= 0) {
+            return false;
+        }
+
+        return true;
     }
 }
